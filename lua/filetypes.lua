2fa6b5e7d7112e66a63fd2b41dc4e836f7ab4cdb
Dmytro-Andrii Kostelnyi <dmitrokostelny95@gmail.com>
2024-02-12 21:24:00 +0200

    Update filetypes
    Fix pattern not detecting target filetype

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -6,8 +6,8 @@ vim.filetype.add({
     plantuml = "plantuml",
   },
   pattern = {
-    [".*\\.мавка"] = "mavka",
-    [".*\\.м"] = "mavka",
+    [".*мавка"] = "mavka",
+    [".*м"] = "mavka",
   },
 })
 
